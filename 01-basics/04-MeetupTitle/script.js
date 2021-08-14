969ecb7ebaebb486be7606d558e77240f65a2804
sustrizh <Sergei.Ustrizhitskii@t-systems.com>
2021-08-14 17:48:42 +0300

    Solve MeetupTitle

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,17 +1,34 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
-function fetchMeetupById(meetupId) {
-  return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      return response.json().then((error) => {
-        throw error;
+const app = createApp({
+  data() {
+    return {
+      currentMeetup: undefined,
+      radio: undefined,
+    };
+  },
+  watch: {
+    radio(newVal) {
+      this.fetchMeetupById(newVal).then((data) => {
+        this.currentMeetup = data;
       });
-    }
-  });
-}
+    },
+  },
+  methods: {
+    fetchMeetupById(meetupId) {
+      return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          return response.json().then((error) => {
+            throw error;
+          });
+        }
+      });
+    },
+  },
+}).mount(`#app`);
 
-// Требуется создать Vue приложение
+window.vm = app;
