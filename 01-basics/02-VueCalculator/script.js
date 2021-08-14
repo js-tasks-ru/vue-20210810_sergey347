596fe5be9c0548d8d738c9aac0fefa3cea1aeced
sustrizh <Sergei.Ustrizhitskii@t-systems.com>
2021-08-14 17:45:09 +0300

    Solve Calculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,35 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const app = createApp({
+  data() {
+    return {
+      firstNumber: 0,
+      secondNumber: 0,
+      chosenRadio: { value: `sum`, operator: '➕' },
+      radioButtons: [
+        { value: `sum`, operator: `➕` },
+        { value: `subtract`, operator: `➖` },
+        { value: `multiply`, operator: `✖` },
+        { value: `divide`, operator: `➗` },
+      ],
+    };
+  },
+  computed: {
+    result() {
+      switch (this.chosenRadio.value) {
+        case `sum`:
+          return +this.firstNumber + +this.secondNumber;
+        case `subtract`:
+          return +this.firstNumber - +this.secondNumber;
+        case `multiply`:
+          return +this.firstNumber * +this.secondNumber;
+        case `divide`:
+          return +this.firstNumber / +this.secondNumber;
+        default:
+          return +this.firstNumber + +this.secondNumber;
+      }
+    },
+  },
+}).mount('#app');
+
+window.vm = app;
